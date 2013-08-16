47c210d2fbfbe30fadd31663e0bfaf0636482c96
Jes Gundy <jes.gundy@threespot.com>
2013-08-16 10:47:57 -0400

    docs removed, css/scss/js reorganized

---
+++ js/config.js
@@ -0,0 +1,23 @@
+(function(root) {
+	
+	var config = {
+		paths: {
+			"jquery": "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min",
+			"jquery.colorbox": "lib/jquery.colorbox",
+			"jquery.scrollto": "lib/jquery.scrollto",
+			"underscore": "lib/underscore"
+		}
+	};
+
+	// IGNORE BELOW THIS LINE...
+	
+	// Enable cache-busting:
+	if ((root.location && root.location.href.indexOf('localhost') >= 0) || root.REQUIRE_NOCACHE) {
+		config.urlArgs = "bust="+(new Date()).getTime();
+	}
+
+	// Predefine RequireJS setup, or configure existing library:
+	if (typeof root.require === 'undefined') root.require = config;
+	else if (root.requirejs && typeof root.requirejs.config === 'function') root.requirejs.config(config);
+	
+}(this));
--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
+// Stand-alone module for non-jquery class/id detection
+define('detection', function() {	
+	// querySelectorAll Polyfill:
+	if (!document.querySelectorAll) {
+		var d=document, s=d.createStyleSheet();
+		d.querySelectorAll = function(r, c, i, j, a) {
+			a=d.all, c=[], r = r.replace(/\[for\b/gi, '[htmlFor').split(',');
+			for (i=r.length; i--;) {
+				s.addRule(r[i], 'k:v');
+				for (j=a.length; j--;) a[j].currentStyle.k && c.push(a[j]);
+				s.removeRule(0);
+			}
+			return c;
+		}
+	}
+	return {
+		// "has()" api: tests for basic CSS selector queries.
+		has: function(selector) {
+			return document.querySelectorAll(selector).length > 0;
+		}
+	};
+});
 
+
+
+define(['detection'], function( detection ) {
+	
+	var mods = [];
+	
+	// Example detection case:
+
+	// if (detection.has('#map-container')) {
+	// 	mods.push('map/main');
+	// }
+
+	require(mods);
+
+});
